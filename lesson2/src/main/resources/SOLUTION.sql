247d7c222c7ad30a807fa6f9c80d5d3d279e61dd
dadabaej <djam388@hotmail.com>
2022-12-29 16:56:56 +0500

    Attempt #3

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,61 +1,61 @@
 INSERT INTO STUDENT
-( name, groupnumber, birthday )
+( name, birthday, groupnumber )
 VALUES
-    ('John', 1, '1974-03-09'),
-    ('Chris', 1, '1975-05-10'),
-    ('Carl', 1, '1976-06-11'),
-    ('Oliver', 2, '1977-07-12'),
-    ('James', 2, '1978-08-13'),
-    ('Lucas', 2, '1979-09-14'),
-    ('Henry', 2, '1980-10-15'),
-    ('Jacob', 3, '1981-11-16'),
-    ('Logan', 3, '1982-12-17'),
-    ('David', 4, '1983-01-18'),
-    ('Boris', 4, '1984-02-19'),
-    ('Helen', 5, '1985-03-20'),
-    ('Albert', 5, '1986-04-21'),
-    ('Harry', 5, '1987-05-22');
+    ('John', '1974-03-09', 1),
+    ('Chris', '1976-06-11', 1),
+    ('Carl', '1976-06-11', 1),
+    ('Oliver', '1977-07-12', 2),
+    ('James', '1978-08-13', 2),
+    ('Lucas', '1979-09-14', 2),
+    ('Henry', '1980-10-15', 2),
+    ('Jacob', '1981-11-16', 3),
+    ('Logan', '1982-12-17', 3),
+    ('David', '1983-01-18', 4),
+    ('Boris', '1984-02-19', 4),
+    ('Helen', '1985-03-20', 5),
+    ('Albert', '1986-04-21', 5),
+    ('Harry', '1987-05-22', 5);
 
 INSERT INTO SUBJECT
-( name, grade )
+( name, description, grade )
 VALUES
-    ('Art', 1),
-    ('Music', 1),
-    ('Geography', 2),
-    ('Music', 2),
-    ('PE', 3),
-    ('Math', 3),
-    ('Science', 4),
-    ('IT', 4),
-    ('Chemistry', 5),
-    ('Biology', 5);
+    ('Art', 'Task 2', 1),
+    ('Music', 'Task 2', 1),
+    ('Geography', 'Task 2', 2),
+    ('Music', 'Task 2', 2),
+    ('PE', 'Task 2', 3),
+    ('Math', 'Task 2', 3),
+    ('Science', 'Task 2', 4),
+    ('IT', 'Task 2', 4),
+    ('Chemistry', 'Task 2', 5),
+    ('Biology', 'Task 2', 5);
 
 
 INSERT INTO PAYMENTTYPE
-( id, name )
+( name )
 VALUES
-    (1, 'DAILY'),
-    (2, 'WEEKLY'),
-    (3, 'MONTHLY');
+    ('DAILY'),
+    ('WEEKLY'),
+    ('MONTHLY');
 
 INSERT INTO PAYMENT
-( student_id, type_id )
+( type_id, amount, payment_date, student_id  )
 VALUES
-    (1, 2),
-    (4, 3),
-    (7, 2),
-    (5, 1),
-    (12, 3),
-    (10, 1),
-    (14, 3);
+    (2, 1000, '2000-01-01', 1),
+    (3, 2000, '2001-02-02', 4),
+    (2, 3000, '2002-03-03', 7),
+    (1, 4000, '2003-04-04', 5),
+    (3, 5000, '2004-05-05', 12),
+    (1, 6000, '2005-06-06', 10),
+    (3, 7000, '2006-07-07', 14);
 
 INSERT INTO MARK
 ( mark, student_id, subject_id )
 VALUES
-    (8, 2, 1),
-    (5, 4, 2),
-    (9, 5, 3),
-    (4, 8, 6),
-    (9, 9, 5),
-    (7, 1, 1),
-    (8, 14, 9);
+    (2, 1, 8),
+    (4, 2, 5),
+    (5, 3, 9),
+    (8, 6, 4),
+    (9, 5, 9),
+    (1, 1, 7),
+    (14, 9, 8);
