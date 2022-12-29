4bff70827f73721be2016cbece4ae4966cd0fb7e
dadabaej <djam388@hotmail.com>
2022-12-30 00:51:01 +0500

    Attempt #7

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,40 @@
-select * from student;
+ALTER TABLE MARK
+    drop CONSTRAINT mark_student_id_fkey;
+
+ALTER TABLE MARK
+    drop CONSTRAINT mark_subject_id_fkey;
+
+ALTER TABLE MARK
+    ADD CONSTRAINT mark_student_id_fkey
+        FOREIGN KEY (student_id)
+            REFERENCES STUDENT
+                (id)
+            ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE MARK
+    ADD CONSTRAINT mark_subject_id_fkey
+        FOREIGN KEY (subject_id)
+            REFERENCES SUBJECT
+                (id)
+            ON DELETE CASCADE ON UPDATE NO ACTION;
+
+
+ALTER TABLE PAYMENT
+    drop CONSTRAINT payment_type_id_fkey;
+
+ALTER TABLE PAYMENT
+    drop CONSTRAINT payment_student_id_fkey;
+
+ALTER TABLE PAYMENT
+    ADD CONSTRAINT payment_type_id_fkey
+        FOREIGN KEY (type_id)
+            REFERENCES PAYMENTTYPE
+                (id)
+            ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE PAYMENT
+    ADD CONSTRAINT payment_student_id_fkey
+        FOREIGN KEY (student_id)
+            REFERENCES STUDENT
+                (id)
+            ON DELETE CASCADE ON UPDATE NO ACTION;
